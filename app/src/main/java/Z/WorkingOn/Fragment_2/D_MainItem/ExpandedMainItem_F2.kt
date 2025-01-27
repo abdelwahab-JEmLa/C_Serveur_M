1e96042f1a2d47132885ac5a7d12f9e2f03dda79
Your Name <abdelwahab.jomla@gmaile.com>
2025-01-27 08:08:20 +0100

    Save

--- app/src/main/java/Z/WorkingOn/Fragment_2/D_MainItem/ExpandedMainItem_F2.kt
+++ app/src/main/java/Z/WorkingOn/Fragment_2/D_MainItem/ExpandedMainItem_F2.kt
@@ -20,7 +20,6 @@ import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.Done
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton

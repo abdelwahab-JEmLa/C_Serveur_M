66c9b479810c2865fc3b37e1eb422c521822ec6c
Your Name <abdelwahab.jomla@gmaile.com>
2025-01-30 04:41:26 +0100

    Savee

---
+++ app/src/main/java/Z_MasterOfApps/Z/Android/Base/App/App/_1/GerantAfficheurGrossistCommend/App/NH_3/id2_TravaillieurListProduitAchercheChezLeGrossist/ViewModel/Extension/ViewModelExtension_App1_F2.kt
@@ -0,0 +1,18 @@
+package Z_MasterOfApps.Z.Android.Packages._1.GerantAfficheurGrossistCommend.App.NH_3.id2_TravaillieurListProduitAchercheChezLeGrossist.ViewModel.Extension
+
+import Z_MasterOfApps.Kotlin.Model._ModelAppsFather.ProduitModel
+import Z_MasterOfApps.Kotlin.ViewModel.ViewModelInitApp
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import kotlinx.coroutines.CoroutineScope
+
+class ViewModelExtension_App1_F2(
+    val viewModel: ViewModelInitApp,
+    val produitsMainDataBase: MutableList<ProduitModel>,
+    val viewModelScope: CoroutineScope,
+) {
+    var afficheProduitsPourRegleConflites by mutableStateOf(false)
+}
+
+

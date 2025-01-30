5c4380c9c9d08c290aa24cf2c4a9e247cc94d046
Your Name <abdelwahab.jomla@gmaile.com>
2025-01-30 04:41:39 +0100

    Savee

---
+++ app/src/main/java/Z_MasterOfApps/Z/Android/Res/XmlsFilesHandler.kt
@@ -0,0 +1,13 @@
+package Z_MasterOfApps.Z.Android.Res
+
+import com.example.clientjetpack.R
+
+class XmlsFilesHandler {
+    companion object {
+        val xmlResources = listOf(
+            Pair("marker_info_window", R.layout.marker_info_window),
+            Pair("info_window_container", R.id.info_window_container),
+            Pair("location_arrow", R.drawable.location_arrow)
+        )
+    }
+}

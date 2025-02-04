6010f990258d9178aa4410ba3105d2377b0817be
Your Name <abdelwahab.jomla@gmaile.com>
2025-02-04 15:31:56 +0100

    Savee

---
+++ app/src/main/java/Z_MasterOfApps/Z/Android/Base/App/App/_1/GerantAfficheurGrossistCommend/App/NH_4/id2_TravaillieurListProduitAchercheChezLeGrossist/A_Id2_TravaillieurListProduitAchercheChezLeGrossist.kt
@@ -0,0 +1,67 @@
+package Z_MasterOfApps.Z.Android.Base.App.App._1.GerantAfficheurGrossistCommend.App.NH_4.id2_TravaillieurListProduitAchercheChezLeGrossist
+
+import Z_MasterOfApps.Kotlin.ViewModel.ViewModelInitApp
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ProgressIndicatorDefaults
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+
+@Composable
+internal fun A_Id2_TravaillieurListProduitAchercheChezLeGrossist(
+    modifier: Modifier = Modifier,
+    viewModelInitApp: ViewModelInitApp = viewModel(),
+) {
+    if (viewModelInitApp.isLoading) {
+        Box(modifier = Modifier.fillMaxSize()) {
+            CircularProgressIndicator(
+                progress = {
+                    viewModelInitApp.loadingProgress
+                },
+                modifier = Modifier.align(Alignment.Center),
+                trackColor = ProgressIndicatorDefaults.circularIndeterminateTrackColor,
+            )
+        }
+        return
+    }
+
+    val databaseSize = viewModelInitApp._modelAppsFather.produitsMainDataBase.size
+
+    val visibleProducts = viewModelInitApp._modelAppsFather.produitsMainDataBase
+        .filter { product ->
+            product.bonCommendDeCetteCota.let {
+                it?.idGrossistChoisi ==
+                        viewModelInitApp.frag2_A1_ExtVM.auFilter
+                        && it
+                    ?.mutableBasesStates
+                    ?.positionProduitDonGrossistChoisiPourAcheterCeProduit!! > 0
+            }
+        }
+
+    Scaffold(
+        modifier = Modifier.fillMaxSize()
+    ) { paddingValues ->
+        Box(modifier = Modifier.fillMaxSize()) {
+            if (databaseSize > 0) {
+                MainList_F2(
+                    initVisibleProducts = visibleProducts,
+                    viewModelInitApp = viewModelInitApp,
+                    paddingValues = paddingValues
+                )
+            }
+        }
+        if (viewModelInitApp
+                ._paramatersAppsViewModelModel
+                .fabsVisibility
+        ) {
+            MainScreenFilterFAB_F2(
+                viewModel = viewModelInitApp,
+            )
+        }
+    }
+}
+
